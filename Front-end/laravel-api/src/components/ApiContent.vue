0fb45703f452b38cacd12d1b9a9667a877dcd1bd
SergioBuzi <sergio.buzi97@gmail.com>
2024-02-28 17:58:30 +0100

    config: cors policy

---
+++ Front-end/laravel-api/src/components/ApiContent.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>HelloWorld</h1>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: 'ApiContent',
+
+        mounted() {
+
+
+            axios.get('http://127.0.0.1:8000/api/v1/test')
+            .then(res => {
+
+                const data = res.data;
+                console.log(data);
+            })
+            .catch(err => {
+                console.err(err);
+            })
+
+        }
+    }
+</script>
